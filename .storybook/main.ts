88cfe8dc2a5fa718cc29f1c96bab0e10a01fb32d
NotTheVeal <rveal@partssource.com>
2026-06-09 17:08:04 -0400

    fix(storybook): disable interactions addon + add rollup external for storybook/test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,23 +5,48 @@ const config: StorybookConfig = {
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        interactions: false,
+      },
+    },
   ],
   framework: '@storybook/react-vite',
   typescript: {
     reactDocgen: false,
   },
-  viteFinal: async (config) => ({
-    ...config,
-    optimizeDeps: {
-      ...config.optimizeDeps,
-      exclude: [
-        ...(config.optimizeDeps?.exclude ?? []),
-        'storybook/test',
-        '@storybook/test',
-      ],
-    },
-  }),
+  viteFinal: async (config) => {
+    const existingExternal = config.build?.rollupOptions?.external;
+    const externalArray = Array.isArray(existingExternal)
+      ? existingExternal
+      : existingExternal
+      ? [existingExternal]
+      : [];
+
+    return {
+      ...config,
+      optimizeDeps: {
+        ...config.optimizeDeps,
+        exclude: [
+          ...(config.optimizeDeps?.exclude ?? []),
+          'storybook/test',
+          '@storybook/test',
+        ],
+      },
+      build: {
+        ...config.build,
+        rollupOptions: {
+          ...config.build?.rollupOptions,
+          external: [
+            ...externalArray,
+            'storybook/test',
+            '@storybook/test',
+          ],
+        },
+      },
+    };
+  },
 };
 
 export default config;
